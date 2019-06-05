aa4640f732d2544ce74b805461f6bfeb445a7fa8
AlexKimov <alex.ea.li@yandex.ru>
2019-06-05 08:53:59 +0300

    add .ani files import script (wip)

---
+++ scripts/3dsmax/cff_clf/build_ani_import_script.bat
@@ -0,0 +1,11 @@
+SET SourceDir=%~dp0
+SET DestDir=%~dp0
+SET Filename=ani_import.zip
+SET MSFilename=ani_import.ms
+SET OutputFolder=Build
+SET ZipArchver=C:\Program Files\7-Zip
+
+call "join_ani_import_files.bat"
+
+CD /D "%ZipArchver%"
+7z.exe a -tzip "%SourceDir%%OutputFolder%\%Filename%" "%DestDir%%OutputFolder%\%MSFilename%"
